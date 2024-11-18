d0d0e5e0161fce8f017f86985bed41c79b4f67d1
darnellbaldwin <baldwidr@mail.uc.edu>
2024-11-18 15:56:07 -0500

    Commit

--- src/SafeInput.java
+++ src/SafeInput.java
@@ -1,19 +1,14 @@
 import java.util.Scanner;
 
 public class SafeInput {
-    /**
-     * @param pipe   a Scanner opened to read from System.in
-     * @param prompt prompt for the user
-     * @return a String response that is not zero length
-     */
 
     public static String getNonZeroLenString(Scanner pipe, String prompt) {
 
-        String retString = ""; // Set this to zero length. Loop runs until it isn't
+        String retString = "";
 
         do {
 
-            System.out.print("\n" + prompt + ": "); // show prompt add space
+            System.out.print("\n" + prompt + ": ");
 
             retString = pipe.nextLine();
 
